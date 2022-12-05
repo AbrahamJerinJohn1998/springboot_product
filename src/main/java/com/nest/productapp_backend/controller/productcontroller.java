daa99438baea9bedc40ba15401667c7b81df30e2
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-12-05 20:26:45 +0530

    initial

---
+++ src/main/java/com/nest/productapp_backend/controller/productcontroller.java
@@ -0,0 +1,14 @@
+package com.nest.productapp_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class productcontroller {
+    @GetMapping("/")
+    public String homepage()
+    {
+        return "Welcome Home";
+    }
+}
